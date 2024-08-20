9338a9afb2797a40256a2d7c6cf01c0c17d65f50
IvanRodriCalleja <ivanrodricalleja@gmail.com>
2024-08-20 17:30:12 +0200

    fix build

--- packages/internationalization/src/locales/es.ts
+++ packages/internationalization/src/locales/es.ts
@@ -1,11 +1,29 @@
 import { type Locales } from '../domain/Locales';
 
-//@ts-expect-error
 const es: Locales = {
 	home: {
 		title: 'Haz tu pdf realidad',
 		description:
 			'Herramientas online gratuitas para fusionar, dividir y comprimir PDFs, además de convertir documentos de Office a PDF y JPG'
+	},
+	mergePdf: {
+		title: 'TODO: Define',
+		description: 'TODO: Define',
+		descriptionImportant: 'TODO: Define',
+		uploadPdf: 'TODO: Define',
+		mergePdfAction: 'TODO: Define'
+	},
+	mergePdfResult: {
+		successfullyMergedTitle: 'TODO: Define',
+		successfullyMergedDescription: 'TODO: Define',
+		fileNameLabel: 'TODO: Define',
+		mergeNewPdf: 'TODO: Define'
+	},
+	shared: {
+		continueWith: 'TODO: Define',
+		pages: 'TODO: Define',
+		download: 'TODO: Define',
+		page: 'TODO: Define'
 	}
 };
 
